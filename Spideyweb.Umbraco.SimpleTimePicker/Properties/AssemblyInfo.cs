c7eb16930661376d807ec72a87128edac994222f
Mark Johnson <mark.johnson@connellsgroup.co.uk>
2020-03-11 11:03:34 +0000

    Add project files.

---
+++ Spideyweb.Umbraco.SimpleTimePicker/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Spideyweb.Umbraco.SimpleTimePicker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Connells Group")]
+[assembly: AssemblyProduct("Spideyweb.Umbraco.SimpleTimePicker")]
+[assembly: AssemblyCopyright("Copyright © Connells Group 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a0152790-85a5-426b-b01b-9d23c89a4ff7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Spideyweb.Umbraco.SimpleTimePicker/Readme.md
@@ -0,0 +1,17 @@
+﻿# Simple Time Picker
+
+A (wait for it..) simple time picker derived from the Umbraco datepicker property editor.
+
+## How to use it
+
+Create a new data type using the 'Simple Time Picker' property editor, save and add to a document type.
+
+The editor will be restricted to using valid 24h (HH:mm) time format and property value is stored as a sting.
+
+## How to install
+
+The package is available via NuGet:
+
+```
+Install-Package [Package Name TBC]
+```
